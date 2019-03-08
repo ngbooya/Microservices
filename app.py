473c76bcbf7bf855d7b864e69e2449defd07acba
williamdalessi <36385898+williamdalessi@users.noreply.github.com>
2019-03-07 20:01:57 -0800

    Update to app.py

--- app.py
+++ app.py
@@ -5,35 +5,30 @@ import os
 
 DATABASE = "./database.db"
 
+
 # Create app
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECRET_KEY'] = 'secret-key'
 
-# check if the database exist, if not create the table and insert a few lines of data
+
 if not os.path.exists(DATABASE):
     conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
-    cur.execute("CREATE TABLE users (title TEXT, body TEXT, date DATE);")
+    #conn.execute("DROP TABLE IF EXISTS articles;")
+    #conn.commit()
+    conn.execute("CREATE TABLE articles (id INTEGER PRIMARY KEY, title TEXT, body TEXT, date DATETIME);")
     conn.commit()
-    cur.execute("INSERT INTO users VALUES('Article1', 'Body text.', '2019-03-05');")
-    cur.execute("INSERT INTO users VALUES('Article2', 'Body text.', '2019-03-05');")
-    #cur.execute("INSERT INTO users VALUES('Jerry', 'Mouse', '40');")
-    #cur.execute("INSERT INTO users VALUES('Peter', 'Pan', '40');")
     conn.commit()
     conn.close()
 
 
-# helper method to get the database since calls are per thread,
-# and everything function is a new thread when called
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-
-# helper to close
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
@@ -41,38 +36,33 @@ def close_connection(exception):
         db.close()
 
 
-@app.route("/")
-def index():
-    cur = get_db().cursor()
-    res = cur.execute("select * from users")
-    return render_template("index.html", users=res)
-
 @app.route("/test", methods = ['GET','POST'])
 def test():
     if request.method=='GET':
         cur = get_db().cursor()
-        res = cur.execute("select * from users")
+        res = cur.execute("select * from articles")
         data = res.fetchall()
         return jsonify(data), 200
     if request.method=='POST':
         conn = get_db()
         cur = conn.cursor()
-        #res = cur.execute("select * from users")
-        res = cur.execute("INSERT INTO users VALUES('Article1', 'Body text.', '2019-03-05');")
+        res = cur.execute("INSERT INTO articles VALUES( " + "NULL"+ ", 'Article1', 'Body text.', date('now'));")
         conn.commit()
-        res = cur.execute("select * from users")
+        res = cur.execute("select * from articles")
         data = res.fetchall()
         return jsonify(data), 201
 
+@app.route("/post", methods = ['GET','POST'])
+def postArticle():
+    if request.method=='POST':
+        content = request.get_json()
+        conn = get_db()
+        cur = conn.cursor()
+        res = cur.execute("INSERT INTO articles VALUES( " + "NULL" + "," + "'" + content['title'] + "'" + "," + "'" + content['body'] + "'" + ", date('now'));")
+        conn.commit()
+        print (content)
+        return jsonify(content), 201
 
-        
-
-
+ 
 if __name__ == "__main__":
-    """
-	Use python sqlite3 to create a local database, insert some basic data and then
-	display the data using the flask templating.
-	
-	http://flask.pocoo.org/docs/0.12/patterns/sqlite3/
-    """
     app.run()
