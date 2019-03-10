2285253418a176e96a43ace73eb1537d911dee10
williamdalessi <36385898+williamdalessi@users.noreply.github.com>
2019-03-09 21:29:42 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 DATABASE = "./database.db"
 
 
-# Create app
+#INITIALIZATION#
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'secret-key'
@@ -15,11 +15,15 @@ app.config['SECRET_KEY'] = 'secret-key'
 if not os.path.exists(DATABASE):
     conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
-    #conn.execute("DROP TABLE IF EXISTS articles;")
-    #conn.commit()
-    conn.execute("CREATE TABLE articles (id INTEGER PRIMARY KEY, title TEXT, body TEXT, date DATETIME);")
+    conn.execute("PRAGMA foreign_keys = ON;")
     conn.commit()
+    conn.execute("CREATE TABLE articles (article_id INTEGER PRIMARY KEY, title TEXT, body TEXT, date DATETIME, user_id INTEGER REFERENCES users)")
+    cur.execute("CREATE TABLE users (user_id INTEGER PRIMARY KEY, email TEXT, password TEXT);")
     conn.commit()
+    cur.execute("CREATE TABLE comments (comment_id INTEGER PRIMARY KEY, comment_text TEXT, date DATETIME, article_id REFERENCES articles);")
+    conn.commit()
+    conn.execute("CREATE TABLE tags (tag_id INTEGER PRIMARY KEY, article_id INTEGER REFERENCES articles, tag TEXT)")
+    conn.commit()   
     conn.close()
 
 
@@ -29,6 +33,7 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
@@ -36,6 +41,7 @@ def close_connection(exception):
         db.close()
 
 
+#TESTING#
 @app.route("/test", methods = ['GET','POST'])
 def test():
     if request.method=='GET':
@@ -46,26 +52,29 @@ def test():
     if request.method=='POST':
         conn = get_db()
         cur = conn.cursor()
-        res = cur.execute("INSERT INTO articles VALUES( " + "NULL"+ ", 'Article1', 'Body text.', datetime('now'));")
+        res = cur.execute("INSERT INTO articles VALUES( " + "NULL"+ ", 'Article1', 'Body text.', datetime('now'), 1);")
         conn.commit()
         res = cur.execute("select * from articles")
         data = res.fetchall()
         return jsonify(data), 201
 
-@app.route("/post", methods = ['POST'])
+
+#ARTICLES#
+
+#POST AN ARTICLE
+@app.route("/article", methods = ['POST'])
 def postArticle():
     if request.method=='POST':
         content = request.get_json()
         conn = get_db()
         cur = conn.cursor()
-        if("title" in content and "body" in content):
-            res = cur.execute("INSERT INTO articles VALUES( " + "NULL" + "," + "'" + content['title'] + "'" + "," + "'" + content['body'] + "'" + ", datetime('now'));")
+        if("title" in content and "body" in content and "user_id" in content):
+            cur.execute("INSERT INTO articles VALUES( " + "NULL" + "," + "'" + content['title'] + "'" + "," + "'" + content['body'] + "'" + ", datetime('now'), " + str(content['user_id']) + " );")
         conn.commit()
-        print (content)
-        return jsonify(content), 201
-
-
-@app.route("/post/<id>", methods = ['GET'])
+        return jsonify({}), 201
+                
+#GET AN ARTICLE
+@app.route("/article/<id>", methods = ['GET'])
 def getArticle(id):
     if request.method=='GET':
         cur = get_db().cursor()
@@ -73,7 +82,8 @@ def getArticle(id):
         data = res.fetchall()
         return jsonify(data), 200
 
-@app.route("/posts/recent/<int:number>", methods = ['GET'])
+#GET THE N MOST RECENT ARTICLES
+@app.route("/articles/recent/<int:number>", methods = ['GET'])
 def getRecentArticle(number):
     if request.method=='GET':
         cur = get_db().cursor()
@@ -82,8 +92,192 @@ def getRecentArticle(number):
                              LIMIT ''' + str(number) + ";")
         data = res.fetchall()
         return jsonify(data), 200
-        
 
- 
+#DELETE AN ARTICLE  
+@app.route("/article/<id>", methods = ['DELETE'])
+def deleteArticle(id):
+    if request.method=='DELETE':
+        conn = get_db()
+        cur = get_db()
+        cur.execute("DELETE FROM articles WHERE id = " + id)
+        conn.commit()
+        return jsonify({}), 200
+        
+#UPDATE AN ARTICLE
+@app.route("/article/<id>/edit",methods=['POST'])
+def editArticle(id):
+    content = request.get_json()
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute('UPDATE articles SET title="' + content['title'] + '" WHERE article_id=' + id + ';')
+    cur.execute('UPDATE articles SET body="' + content['body'] + '" WHERE article_id=' + id + ';')
+    cur.execute("UPDATE articles SET date=datetime('now') WHERE article_id= " + id )
+    conn.commit()
+    conn.close()
+    return jsonify({}), 200
+
+#RETRIEVE METADATA FOR N MOST RECENT ARTICLES
+@app.route("/articles/recent/metadata/<int:number>", methods = ['GET'])
+def getRecentArticleMetaData(number):
+    if request.method=='GET':
+        cur = get_db().cursor()
+        res = cur.execute('''SELECT title, body, user_id, date, article_id FROM articles 
+                             ORDER BY date DESC
+                             LIMIT ''' + str(number) + ";")
+        data = res.fetchall()
+        return jsonify(data), 200
+
+
+
+
+#USERS#
+
+#CREATE A USER
+@app.route("/users/create", methods = ['POST'])
+def createUser():
+    if request.method == 'POST':
+        content = request.get_json()
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users VALUES( " + "NULL" + "," + "'" + content['email'] + "', " + "'" + content['password'] + "'"  + " );")
+        conn.commit()
+        show = cur.execute("SELECT * FROM users")
+        data = show.fetchall()
+        print(data)
+        conn.close()
+        return jsonify(data), 201
+
+#CHAGE THE PASSWORD OF A USER
+@app.route("/users/changepassword",methods=['POST'])
+def changePassword():
+    content = request.get_json()
+    conn = get_db()
+    cur = conn.cursor()
+    # cur.execute('SELECT * FROM users WHERE EXISTS(DELETE FROM users WHERE user_id = 1)')
+    cur.execute('UPDATE users SET password="' + str(content['password']) + '" WHERE user_id=' + str(content['user_id']) + ';')
+    #cur.execute('UPDATE users SET password="thispasshasbeenchangedAGAIN" WHERE user_id=3')
+    conn.commit()
+    conn.close()
+    return jsonify({}), 200
+
+
+
+
+#COMMENTS#
+
+#POST A COMMENT TO AN ARTICLE
+@app.route("/articles/<int:article_number>/comment/add", methods = ['POST'])
+def postComment(article_number):
+    if request.method=='POST':
+        content = request.get_json()
+        if("comment_text" in content and "article_id" in content):           
+            conn = get_db()
+            cur = conn.cursor()
+            cur.execute("INSERT INTO comments VALUES( NULL," + "'" + content['comment_text'] +  "'" + ", datetime('now'), "  + content['article_id']  +  ")")
+            conn.commit()
+            cur.close()
+            return jsonify({}), 200
+        return jsonify({}), 409
+        
+#RETRIEVE THE N MOST RECENT COMMENTS TO AN ARTICLE       
+@app.route("/articles/<int:article_number>/comments/<int:numComments>", methods = ['GET'])
+def getRecentComments(article_number, numComments):
+    if request.method=='GET':
+        conn = get_db()
+        conn.row_factory = sqlite3.Row
+        cur = get_db().cursor()
+        res = cur.execute('''SELECT * FROM comments WHERE article_id=''' + "'" + str(article_number) + "'" +  '''ORDER BY date DESC LIMIT ''' + str(numComments) + ";")
+        data = res.fetchall()
+        return jsonify(data), 200
+
+#COUNT THE NUMBER OF COMMENTS FOR A GIVEN ARTICLE
+@app.route("/articles/<int:article_number>/comments/count", methods = ['GET'])
+def countArticleComments(article_number):
+    if request.method=='GET':
+        cur = get_db().cursor()
+        res = cur.execute('SELECT article_id, COUNT(article_id) FROM comments GROUP BY article_id')
+        data = res.fetchall()
+        return jsonify(data), 200
+
+#DELETE AN INDIVIDUAL COMMENT
+@app.route("/comments/<int:comment_id>", methods = ['DELETE'])
+def deleteComment(comment_id):
+    if request.method=='DELETE':
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute("DELETE FROM comments WHERE comment_id = " + str(comment_id))
+        conn.commit()
+        return jsonify({}), 200
+
+#TAGS#
+
+#ADD TAG TO AN ARTICLE
+@app.route("/articles/<article_number>/tags/create", methods = ['POST'])
+def tagArticle(article_number):
+    if request.method=='POST':
+        content = request.get_json()
+        conn = get_db()
+        cur = conn.cursor()
+        if("tag" in content):
+            cur.execute("INSERT INTO tags VALUES( " + "NULL" + "," + "'" + str(article_number) + "'" + "," + "'" + content['tag'] + "'" + ')')
+        conn.commit()
+        return jsonify({}), 201
+
+#RETRIEVE TAGS FOR AN ARTICLE
+@app.route("/articles/<article_number>/tags", methods = ['GET'])
+def getArticleTags(article_number):
+    if request.method=='GET':
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute("SELECT tag FROM tags WHERE  article_id =" + article_number + ";")
+        res = cur.fetchall()
+        mergelist = []
+        for list in res:
+            mergelist += list            
+        return jsonify(mergelist), 201
+
+#DELETE TAG FROM AN ARTICLE
+@app.route("/article/<artNum>/tags/<tag>/delete", methods= ['DELETE'])
+def deleteTagFromArticle(artNum, tag):
+    if request.method == 'DELETE':
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute("DELETE FROM tags WHERE article_id = '" + artNum + "' AND tag = '" + tag + "'")
+        conn.commit()
+        return jsonify({}), 200 
+
+#DELETE ONE OR MORE TAGS FROM AN ARTICLE
+# JSON IS IN THIS FORMAT:
+# {
+#     "tag1": ""
+#     "tag2": ""
+# }
+@app.route("/article/<artNum>/tags/delete", methods= ['DELETE'])
+def deleteTagsFromArticle(artNum):
+    if request.method == 'DELETE':
+        content = request.get_json()
+        conn = get_db()
+        cur = conn.cursor()
+        for key in content:            
+            value = content[key]        
+            cur.execute("DELETE FROM tags WHERE article_id = '" + artNum + "' AND tag = '" + value + "'")
+        conn.commit()
+        return jsonify({}), 200 
+
+#RETRIEVE A LIST OF ARTICLES WITH A GIVEN TAG
+@app.route("/tag/<tag>/allarticles", methods = ['GET'])
+def getArticleListForTags(tag):
+    if request.method=='GET':
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute("SELECT article_id FROM tags WHERE  tag = '" + tag + "';")
+        res = cur.fetchall()
+        mergelist = []
+        for list in res:
+            mergelist += list            
+        return jsonify(mergelist), 201
+
+
+#APP RUN
 if __name__ == "__main__":
     app.run()
